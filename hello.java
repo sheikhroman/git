eb2ecadd197d7739a2904ba0b66008f8fe46e356
Roman <sheikhroman042@gmail.com>
2021-03-23 16:21:01 +0600

    add new mathod in Hello.java

--- hello.java
+++ hello.java
@@ -2,4 +2,8 @@ public class Hello{
     public static viod main(String[] args){
         System.out.printIn("VS code is Awesome....");
     }
+    greeting("Roman");
+}
+public static void greeting(String name){
+    System.out.printIn("Wellcome " .name);
 }
