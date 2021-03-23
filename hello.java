633d60b07780e7f2158d65fb5ec4cb3dc5ce922b
Roman <sheikhroman042@gmail.com>
2021-03-23 18:54:09 +0600

    new mathod sqr

--- hello.java
+++ hello.java
@@ -2,8 +2,11 @@ public class Hello{
     public static viod main(String[] args){
         System.out.printIn("VS code is Awesome....");
     }
-    greeting("Roman");
+    greeting("Git";)
 }
 public static void greeting(String name){
-    System.out.printIn("Wellcome " .name);
+    System.out.printIn("BY By " .name);
+}
+public static int sqr(int x) {
+    return x*x;
 }
