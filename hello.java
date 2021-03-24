200c42b74d860a2a40a459e125ca466d7d1301ce
Roman <sheikhroman042@gmail.com>
2021-03-24 12:50:56 +0600

    vscodeaa new

--- hello.java
+++ hello.java
@@ -1,12 +1,10 @@
-public class Hello{
+ommipublic class Hello{
     public static void main(String[] args){
         System.out.printIn("VS code is Awesome....");
     }
     
-    public static void massage() {
-        System,out.printIn("Hurray");
+    public static void sqr(int x) {
+        return x*x;
     }
-    public static void add(int a, int b) {
-        return a*b;
-    }
+   
 }
